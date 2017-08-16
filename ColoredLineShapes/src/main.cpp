87c233b12761e0e7b91d058c8caf36e4d6e7a0a8
Victor Gaiva <victorgaiva2014@gmail.com>
2017-08-16 13:23:20 -0400

    Removed WeightedLines since there are unecessary

--- ColoredLineShapes/src/main.cpp
+++ ColoredLineShapes/src/main.cpp
@@ -3,19 +3,15 @@
 
 //User libs includes
 #include "LineShapes.h"
-#include "WeightedLine.h"
 
 void handleEvents(sf::RenderWindow *);
 int main()
 {
     sf::Color backgroundColor(230,230,230,255);    
     sf::RenderWindow window(sf::VideoMode(800, 600), "LineShapes");
-    class WeightedLine line1(50.0, 50.0, 100.0, 100.0, 1.0, sf::Color(sf::Color::Black));
-    class WeightedLine line2(100.0, 100.0, 150.0, 25.0, 8.0, sf::Color(sf::Color::Black));
 
     // change the position of the window (relatively to tmahe desktop)
     window.setPosition(sf::Vector2i(10, 50));
-
     int i = 0;
 
     //Setting up
@@ -29,8 +25,6 @@ int main()
         window.clear(backgroundColor);
         
         //draw the lines
-        window.draw(line1);
-        window.draw(line2);
 
         //finish frame
         window.display();
