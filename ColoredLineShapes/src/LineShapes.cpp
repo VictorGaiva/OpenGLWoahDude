3af2d1ed886f0568ab2790527f3f0909357ad1b3
Victor Gaiva <victorgaiva2014@gmail.com>
2017-08-22 11:47:57 -0400

    Progress but shit don't compile

--- ColoredLineShapes/src/LineShapes.cpp
+++ ColoredLineShapes/src/LineShapes.cpp
@@ -1,2 +1,13 @@
 #include "LineShapes.h"
 
+LineShapes::LineShapes(sf::Vector2f _pos, float _radius, unsigned int _count){
+    for (int i = 0; i < _count; i++){
+        double x = _radius * cos((TWO_PI/_count)*i);
+        double y = _radius * sin((TWO_PI/_count)*i);
+        this->vertices.append(sf::Vertex(sf::Vector2f(x, y)));
+    }
+}
+
+void draw(sf::RenderTarget & target, sf::RenderStates states){
+
+}
